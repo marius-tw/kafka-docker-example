b18d029a123b2e52860dd948982c58e5c8e95003
Kelsey Mok <kelseymok@gmail.com>
2022-07-18 16:18:44 +0200

    echo done command

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,5 @@
 set -ex
 nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_CONFIG/zookeeper.properties &
 nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_CONFIG/server.properties &
+
+echo "DONE. Type your next command."
